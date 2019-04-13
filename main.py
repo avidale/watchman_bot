e5658c2032769704eb43b4698eae9cd6f15620b0
David Dale <ddale@yandex-team.ru>
2019-04-13 19:28:43 +0300

    migrate to mongo

--- main.py
+++ main.py
@@ -3,12 +3,10 @@
 import argparse
 import telebot
 import os
-import time
 import random
 from datetime import datetime
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from pymongo import MongoClient
 
 ON_HEROKU = os.environ.get('ON_HEROKU')
 TOKEN = os.environ['TOKEN']
@@ -17,11 +15,27 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 TELEBOT_URL = 'telebot_webhook/'
 BASE_URL = 'https://the-watchman-bot.herokuapp.com/'
-DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///watchman_local.db')
 
-server.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-db = SQLAlchemy(server)
-migrate = Migrate(server, db)
+
+MONGO_URL = 'mongodb://watchman_user1:watchman_password1@ds137441.mlab.com:37441/heroku_5c9l9qp5'
+mongo_client = MongoClient(MONGO_URL)
+mongo_db = mongo_client.get_default_database()
+mongo_users = mongo_db.get_collection('users')
+mongo_messages = mongo_db.get_collection('messages')
+
+
+def try_insert_user(tg_bot_user):
+    found = mongo_users.find_one({'tg_id': tg_bot_user.id})
+    if found is not None:
+        return
+    mongo_users.insert_one(dict(
+        tg_id=tg_bot_user.id,
+        first_name=tg_bot_user.first_name,
+        last_name=tg_bot_user.last_name,
+        username=tg_bot_user.username,
+        subscribed=True  # todo: ask for subscription
+    ))
+
 
 THE_QUESTIONS = [
     "Остановись!",
@@ -33,25 +47,6 @@ with open('many_questions.txt', 'r', encoding='utf-8') as f:
     LONGLIST = list(f.readlines())
 
 
-class StoredMessage(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Unicode())
-    user_id = db.Column(db.Integer)
-    from_user = db.Column(db.Boolean)
-    process_time = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Message "{}" {} "{}" at {}>'.format(self.text, "from" if self.from_user else "to", self.user_id, self.process_time)
-        
-        
-class StoredUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, unique=True)
-
-    def __repr__(self):
-        return '<User "{}">'.format(self.user_id)
-
-
 @server.route("/" + TELEBOT_URL)
 def web_hook():
     bot.remove_webhook()
@@ -61,9 +56,12 @@ def web_hook():
 
 @server.route("/wakeup/")
 def wake_up():
-    web_hook()
-    for user in StoredUser.query.all():
-        print("Writing to user '{}'".format(user.user_id))
+    # web_hook()
+    for user in mongo_users.find():
+        user_id = user.get('tg_id')
+        if not user_id or not user.get('subscribed'):
+            print("Do not write to user '{}'".format(user))
+        print("Writing to user '{}'".format(user_id))
         """
         for utterance in THE_QUESTIONS:
             bot.send_message(user.user_id, utterance)
@@ -72,59 +70,31 @@ def wake_up():
             db.session.add(msg)
         """
         utterance = random.choice(LONGLIST)
-        bot.send_message(user.user_id, utterance)
-        #msg = StoredMessage(text=utterance, user_id=user.user_id, from_user=False)
-        #db.session.add(msg)
-    db.session.commit()
+        bot.send_message(user_id, utterance)
+
+        msg = dict(text=utterance, user_id=user_id, from_user=False, timestamp=str(datetime.utcnow()))
+        mongo_messages.insert_one(msg)
     return "Маам, ну ещё пять минуточек!", 200
 
 
 @bot.message_handler(func=lambda message: True)
 def process_message(message):
-    #msg = StoredMessage(text=message.text, user_id=message.chat.id, from_user=True)
-    #print(msg)
-    #db.session.add(msg)
+    try_insert_user(message.from_user)
+    msg = dict(text=message.text, user_id=message.chat.id, from_user=True, timestamp=str(datetime.utcnow()))
+    mongo_messages.insert_one(msg)
     if message.text == 'вопросик пожалуйста':
         response = random.choice(LONGLIST)
     else:
         response = "Я пока что не обладаю памятью, но я буду писать вам каждый вечер. Это моя работа."
-    #db.session.add(StoredMessage(text=response, user_id=message.chat.id, from_user=False))
+    msg = dict(text=response, user_id=message.chat.id, from_user=False, timestamp=str(datetime.utcnow()))
+    mongo_messages.insert_one(msg)
     bot.reply_to(message, response)
-    #print('total number of messages: {}'.format(
-    #    len(StoredMessage.query.all()))
-    #)
-    #found_user = StoredUser.query.filter_by(user_id=message.chat.id).first()
-    #if found_user:
-    #    print("found existing user '{}'".format(found_user))
-    #else:
-    #    new_user = StoredUser(user_id=message.chat.id)
-    #    #db.session.add(new_user)
-    #    print("added user '{}'".format(new_user))
-    #db.session.commit()
 
 
 @server.route('/' + TELEBOT_URL + TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
-
-
-def main_old():
-    db.create_all()
-    if ON_HEROKU:
-        # get the heroku port
-        port = int(os.environ.get('PORT', 17995))
-        host = '0.0.0.0'
-    else:
-        host = '127.0.0.1'
-        port = 5000
-    if ON_HEROKU:
-        print("running flask on the port {}".format(port))
-        server.run(host=host, port=port)
-        web_hook()
-    else:
-        bot.remove_webhook()
-        bot.polling()
 
 
 parser = argparse.ArgumentParser(description='Run the bot')
@@ -143,4 +113,4 @@ def main_new():
 
 
 if __name__ == '__main__':
-    main_old()
+    main_new()
