0acfeb6a97af17039bc9dc6af51e160efe35c47e
David Dale <ddale@yandex-team.ru>
2019-05-11 17:18:13 +0300

    fix get state

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -53,7 +53,7 @@ def make_suggests(text='', intent=Intents.OTHER, user_object=None):
         texts.append('Отписаться'),
     else:
         texts.append('Подписаться')
-    if user_object.get('coach_state').get('is_active'):
+    if user_object.get('coach_state', {}).get('is_active'):
         texts.append('Завершить сессию')
     else:
         texts.append('Хочу коуч-сессию')
