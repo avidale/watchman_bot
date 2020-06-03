15515dbb87dae8da4029f7128964f0d2bce1b8c9
David <dale.david@mail.ru>
2020-06-03 21:50:53 +0300

    fix inflector

--- morphology.py
+++ morphology.py
@@ -6,10 +6,12 @@ def gent(word):
     parses = morph.parse(word)
     if not parses:
         return word
-    result = parses[0].inflect({'gent'}).word
+    inflected = parses[0].inflect({'gent'})
+    if not inflected:
+        return word
+    result = inflected.word
     if word.istitle():
         result = result[0].upper() + result[1:]
     elif word.isupper():
         result = result.upper()
     return result
-
